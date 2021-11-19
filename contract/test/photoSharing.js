8fe824b1d852e5e766105a40b4f1c80f576b2b24
JOE MON <56827756+JOEMN96@users.noreply.github.com>
2021-11-19 21:53:12 +0530

    tests

--- contract/test/photoSharing.js
+++ contract/test/photoSharing.js
@@ -1,13 +1,18 @@
 let photoSharing = artifacts.require('photoSharing');
 
 contract('photoSharing', () => {
-  let contract;
+  let contract = null;
   before(async () => {
-    contract = await contract.deployed();
+    contract = await photoSharing.deployed();
   });
 
-  it('It should be deployed', () => {
+  it('Should Deploy', async () => {
     console.log(contract);
     assert(contract.address != '');
+  });
+
+  it('Should return name', async () => {
+    let res = await contract.name();
+    assert(res === 'Share Photos');
   });
 });
