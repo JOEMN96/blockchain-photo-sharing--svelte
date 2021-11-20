006d450840a8080233e3f41a149f426f30fdc8f2
JOE MON <56827756+JOEMN96@users.noreply.github.com>
2021-11-20 16:06:36 +0530

    UI

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,75 +1,12 @@
 <script>
-	import Button from './button.svelte'
-
-	let arr = [
-		{id:1,name:"osima"},
-		{id:2,name:"julian"},
-		{id:3,name:"pepe"},
-	]
-	 let name = "Joe";
-	 let clr = "green"
-	 const handleClick = () => {
-		 name = "Mon" 
-	 }
-	 const handeleIp = (e) =>{
-		 name = e.target.value
-		
-	 }
-	 $:{
-		 console.log(name);
-	  }
-
-	 $:reactive = name + clr;
+	import Header from "./components/Header.svelte";
 </script>
+
 <main>
-	<h1 style="color:{clr}" class:test={true} >Hello {name}!</h1>
-	<p>{reactive}</p>
-	<button  on:click={handleClick}>Click Me</button>
-	<input  on:input={handeleIp} type="text"  bind:value={name}>
-
-	{#each arr as item (item.id)}
-		<p>{item.name}</p>
-		<Button prop={item.name} />
-
-		{:else}
-			<p>No data Available</p>
-	{/each}
-
-
-	{#each arr as item (item.id)}
-		<p>{item.id}</p>
-		{:else} 
-		<p>No data</p>
-	{/each}
-
-	{#if name == "Joe"}
-		<h1>Hey Joe</h1>
-		{:else if name == "mon"}
-			<p>Hi MOon</p>
-		{:else}
-		<h2>Hey GGs</h2>
-	{/if}
+	<Header/>
 
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none; 
-		}
-	}
 </style>
---
+++ frontend/src/components/Header.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<header>
+    <nav>
+        <h1>Insta</h1>
+    </nav>
+</header>
+
+<style>
+    nav {
+        padding: 10px;
+        background-color: #012650;
+        color: white;
+    }
+</style>
